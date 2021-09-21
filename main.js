6898b867bca51b3d13a5a79fe71a091a16e0d618
atul-mandavkar <76526334+atul-mandavkar@users.noreply.github.com>
2021-09-21 20:12:48 +0530

    3rd try
    This time adding a javascript with express

---
+++ main.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+
+console.log(__dirname);
+app.use(express.static(path.join(__dirname, "views")));
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
+var port = process.env.PORT || 3000
+app.listen(port, ()=>console.log("Listening to port " + port));
